2d01ef505a527580c6587501f67c4d24b20333fe
Ticicas <hei.Ticicas@gmail.com>
2026-08-19 15:26:58 +0300

    fix: enable CORS to allow frontend requests

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-
-
 import express, { Application } from "express";
+import cors from "cors";
 import studentRoutes from "./Controller/studentController";
 import userRoutes from "./Controller/userController";
 
+
 const app: Application = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/students", studentRoutes);
